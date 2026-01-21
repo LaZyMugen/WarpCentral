53c972aef6dd4f76382ebb4d9cc2ef0ab1b6a17c
LaZyMugen <shaswatsahoo234@gmail.com>
2026-01-21 11:03:49 +0530

    sometimes all we need in our life is a penguin going to the mountains

--- internal/chunker/chunker.go
+++ internal/chunker/chunker.go
@@ -14,3 +14,25 @@ type Chunk struct{
 	if totalSize <= 0 {
 		return nil
   }
+
+  chunks := make([]Chunk, 0, parts)
+	partSize := totalSize / int64(parts)
+	rem := totalSize % int64(parts)
+
+	var start int64 = 0
+	for i := 0; i < parts; i++ {
+		size := partSize
+		if int64(i) < rem {
+			size++
+		}
+		end := start + size - 1
+		chunks = append(chunks, Chunk{
+			ID:    i,
+			Start: start,
+			End:   end,
+		})
+		start = end + 1
+	}
+
+	return chunks
+}
